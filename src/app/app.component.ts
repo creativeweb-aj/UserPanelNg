ea7c7c57f57b1714ef05f2f64e7517561f264567
Ajay kumar sharma <48996332+creativeweb-aj@users.noreply.github.com>
2020-05-31 18:30:24 +0530

    app css file extension set in ts file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import {AuthServicesService} from './auth-services.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Users Panel';
